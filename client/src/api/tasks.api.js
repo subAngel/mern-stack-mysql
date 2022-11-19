b5b2744dab9002aa49793779890e4ba7bf3d6538
subAngel <whoangel.agl@gmail.com>
2022-11-19 00:31:56 -0600

    List tasks frontend

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -2,3 +2,7 @@ import axios from "axios";
 export const createTaksRequest = async (task) => {
 	return await axios.post("http://localhost:9000/tasks", task);
 };
+
+export const getTasksRequest = async () => {
+	return await axios.get("http://localhost:9000/tasks");
+};
--- client/src/pages/TaksPage.jsx
+++ client/src/pages/TaksPage.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+import { getTasksRequest } from "../api/tasks.api.js";
+import TaskCard from "../components/TaskCard";
+
 function TaksPage() {
-	return <div>Home</div>;
+	const [tasks, setTasks] = useState([]);
+	useEffect(() => {
+		async function loadTasks() {
+			const response = await getTasksRequest();
+			setTasks(response.data);
+		}
+		loadTasks();
+	}, []);
+
+	return (
+		<div>
+			<h1>Tasks</h1>
+			{tasks.map((task) => (
+				<TaskCard task={task} key={task.id} />
+			))}
+		</div>
+	);
 }
 
 export default TaksPage;
