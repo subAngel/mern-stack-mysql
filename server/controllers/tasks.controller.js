cffe82bae57e1a5cb7877330342622535d12332e
subAngel <whoangel.agl@gmail.com>
2022-11-18 22:09:18 -0600

    Server routes

---
+++ server/controllers/tasks.controller.js
@@ -0,0 +1,15 @@
+export const getTasks = (req, res) => {
+	res.send("obteniendo tareas");
+};
+export const getTask = (req, res) => {
+	res.send("obteniendo tarea");
+};
+export const createTask = (req, res) => {
+	res.send("creando tarea");
+};
+export const updateTask = (req, res) => {
+	res.send("actualizando tarea");
+};
+export const deleteTask = (req, res) => {
+	res.send("elimnando tarea");
+};
--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { PORT } from "./config.js";
 import indexRoutes from "./routes/index.routes.js";
+import taskroutes from "./routes/tasks.routes.js";
 
 const app = express();
 
 // * Settings
 
 app.use(indexRoutes);
+app.use(taskroutes);
 
 app.listen(PORT);
 console.log(`Server running on http://localhost:${PORT}}`);
